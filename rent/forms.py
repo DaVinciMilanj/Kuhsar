40e0f45d1792eeff295316829d29f691ab1bcb87
DaVinciMilanj <radinmilani2005@gmail.com>
2024-09-03 11:03:21 +0330

    complte

--- rent/forms.py
+++ rent/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import RentRoom
 from django_jalali.forms import jDateField, jDateInput
-from django_jalali import forms as jalili_form
+from django_jalali.admin.widgets import AdminjDateWidget
+
+
 
 
 
 class RentRoomForm(forms.ModelForm):
+    start_date = jDateField(widget=AdminjDateWidget)
+    end_date = jDateField(widget=AdminjDateWidget)
+    best_date = jDateField(widget=AdminjDateWidget)
+    golden_date = jDateField(widget=AdminjDateWidget, required=False)
     class Meta:
         model = RentRoom
         fields = ['user', 'price', 'discount', 'start_date', 'end_date', 'best_date', 'golden_date']
